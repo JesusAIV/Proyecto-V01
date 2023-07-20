97732a5565918f416c884ee7cac9bf5580c9e8b8
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-07-19 22:54:11 -0500

    Se agregó los links y scripts

--- view/index.php
+++ view/index.php
@@ -15,8 +15,12 @@ $titulo = $configC->titleWeb();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $titulo ?></title>
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css">
+    <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <link rel="stylesheet" href="<?php echo $configC->rutaCss() ?>global.css">
+    <script src="<?php echo $configC->rutaJs() ?>sidebar.js"></script>
 </head>
-<body>
+<body id="body-pd">
     <?php include $configC->showPage(0); ?>
 </body>
 </html>
