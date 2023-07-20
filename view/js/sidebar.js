bcab32a333d9cdf3e2fa1a60557740dc4d29cdb7
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-07-19 22:51:54 -0500

    Archivo JS para el sidebar

---
+++ view/js/sidebar.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) => {
+        const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId),
+            bodypd = document.getElementById(bodyId),
+            headerpd = document.getElementById(headerId)
+
+        // Validate that all variables exist
+        if (toggle && nav && bodypd && headerpd) {
+            toggle.addEventListener('click', () => {
+                // show navbar
+                nav.classList.toggle('section_sidebar-show')
+                // change icon
+                toggle.classList.toggle('fa-xmark')
+                // add padding to body
+                bodypd.classList.toggle('body-pd')
+                // add padding to header
+                headerpd.classList.toggle('body-pd')
+            })
+        }
+    }
+
+    showNavbar('header-toggle', 'nav-bar', 'body-pd', 'header-sidebar')
+
+});
