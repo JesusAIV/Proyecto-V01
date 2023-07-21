86ba401ec0513d58270518f564ebebd48be7338b
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-07-21 00:36:30 -0500

    Agregando cdn de Jquery y ChartJS, e incluyendo el sidebar

--- view/index.php
+++ view/index.php
@@ -17,10 +17,19 @@ $titulo = $configC->titleWeb();
     <title><?php echo $titulo ?></title>
     <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.4.0/css/all.css">
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-regular-rounded/css/uicons-regular-rounded.css'>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="<?php echo $configC->rutaCss() ?>global.css">
     <script src="<?php echo $configC->rutaJs() ?>sidebar.js"></script>
 </head>
 <body id="body-pd">
-    <?php include $configC->showPage(0); ?>
+
+    <?php include 'layout/sidebar_employees.php' ?>
+    <div class="body_sidebar">
+        <?php include $configC->showPage(0); ?>
+    </div>
+
+    <script src="<?php echo $configC->rutaJs() ?>grafico.js"></script>
+
 </body>
 </html>
