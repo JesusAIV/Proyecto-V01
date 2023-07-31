5a0e25d4c405912ac0b5bfa67f5d867a6e8be15a
YdaliaS <shellyjoaquinc@gmail.com>
2023-07-30 23:50:18 -0500

    Constantes de la base de datos

--- config/constantes.php
+++ config/constantes.php
@@ -25,3 +25,28 @@ define('ID_ROLE_EMPLOYEES', $envVariables['ID_ROLE_EMPLOYEES']);
  * ID del rol de los clientes
  */
 define('ID_ROLE_CUSTOMERS', $envVariables['ID_ROLE_CUSTOMERS']);
+
+/**
+ * host de la base de datos
+ */
+define('DB_HOST', $envVariables['DB_HOST']);
+
+/**
+ * puerto de la base de datos
+ */
+define('DB_PORT',$envVariables['DB_PORT']);
+
+/**
+ * nombre de la base de datos
+ */
+define('DB_DATABASE',$envVariables['DB_DATABASE']);
+
+/**
+ * nombre de usuario de la base de datos
+ */
+define('DB_USERNAME',$envVariables['DB_USERNAME']);
+
+/**
+ *  contraseña de la base de datos
+ */
+define('DB_PASSWORD',$envVariables['DB_PASSWORD']);
