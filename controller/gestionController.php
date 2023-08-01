21f6595886991ab295bdd0685331159b6b7cf18d
JesusAIV <jesusalbertoisiquevasquez@gmail.com>
2023-08-01 12:50:36 -0500

    extendido de la clase mainController / manejo de errores con try_catch

--- controller/gestionController.php
+++ controller/gestionController.php
@@ -2,32 +2,43 @@
 
 if ($xhr){
     require_once "../../config/connection.php";
+    require_once "../../controller/mainController.php";
 } else {
     require_once "./config/connection.php";
+    require_once "./controller/mainController.php";
 }
 
 /**
  * Clase controlador para la gestion general del sistema
  */
-class gestionController
+class gestionController extends mainController
 {
 
     /**
      * Cuenta el total de registros en una tabla
      *
      * @param string $tabla
-     * @return array $cantidad
+     * @return string $respuesta
      */
     public function cantidadRegistros($tabla){
 
-        $conn = new Connection();
-        $conn = $conn->connect();
+        try {
+            $conn = new Connection();
+            $conn = $conn->connect();
 
-        $sql = "SELECT COUNT(*) AS cantidad FROM {$tabla}";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $cantidad = $stmt->fetchAll();
+            $sql = "SELECT COUNT(*) AS cantidad FROM {$tabla}";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $cantidad = $stmt->fetchAll();
 
-        return $cantidad;
+            $respuesta = $cantidad[0]->cantidad;
+
+            return $respuesta;
+        } catch (Exception $e){
+            $respuesta = "not found";
+
+            return $respuesta;
+        }
+
     }
 }
